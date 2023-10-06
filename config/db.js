79a73681ae26913ad4e317df62b2de30dbf3368b
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-10-06 14:37:42 +0500

    changes

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose")
 
+const DB_URI="mongodb+srv://aliansari2566:Alidb123@cluster0.llqwkek.mongodb.net/mynodedb?retryWrites=true&w=majority"
+
 const connectDB = async () =>{
     try {
         await mongoose.connect(DB_URI)
+        console.log("Database connected");
     } catch (error) {
         console.log("Error while connect" + error.message);
     }
 }
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,32 +1,76 @@
 const express = require("express");
+const Hero = require("./models/heromodel")
 const app = express();
+const connectDB = require("./config/db");
+connectDB();
 
+
+
+app.use(express.json());
 //API ROUTES
 
-app.get("/",(req,res)=>{
-    // res.send("hello word ?"); 
-    res.json({msg: 'API server is running'} );
-})
-app.get("/superheros",(req,res)=>{
-    res.json({msg: 'showing alll heros'} );
+// app.get("/",(req,res)=>{ 
+
+//     res.json({msg: 'API server is running'} );
+// })
+app.get("/superheros",async (req,res)=>{
+  try {
+    const heros = await Hero.find({})
+    res.json(heros);
+  } catch (error) {
+    console.error("An error occurred.", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
 })
 
-// :superHeroID You have defined :superHeroId as a route parameter by including a colon : before it. This tells Express that superHeroId is a dynamic part of the URL, 
-app.get("/superheros/:superHeroId",(req,res)=>{
-    res.json({msg: 'Super hero ID ' + req.params.superHeroId} );
+// :superHeroID You have defined :superHeroId as a route parameter by including a colon : before it. This tells Express that superHeroId is a dynamic part of the UR L, 
+app.get("/superheros/:superHeroId",async  (req,res)=>{
+    // res.json({msg: 'Super hero ID ' + req.params.superHeroId} );
+    try {
+        const hero =await Hero.findById(req.params.superHeroId)
+        res.json(hero)
+       } catch (error) {
+        console.log(error);
+       }
 })
 
 
-app.post("/superheros",(req,res)=>{
-    res.json({msg: "Creating Superhero" } );
+app.post("/superheros",async (req,res)=>{
+    try {
+        await Hero.create({
+             superheroname: req.body.superheroname,
+             name: req .body.name,
+        })
+        res.json({msg: "Superhero created" } );
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 // we need id in this put request because we'll have to edit one super hero by id
-app.put("/superheros/:superHeroId",(req,res)=>{
-    res.json({msg: "Editing  Superhero " + req.params.superHeroId} );
+app.put("/superheros/:superHeroId",async (req,res)=>{
+    // res.json({msg: "Editing  Superhero " + req.params.superHeroId} );
+    try {
+         await Hero.findByIdAndUpdate(req.params.superHeroId,{
+            superheroname: req.body.superheroname,
+            name: req .body.name,
+         })
+         res.status(200).json({msg:"Hero is updated"})
+    }   catch (error) {
+        console.log(error);
+    }
+    
 })
 
-app.delete("/superheros/:superHeroId",(req,res)=>{
-    res.json({msg: "Deleting Superhero " + req.params.superHeroId} );
+app.delete("/superheros/:superHeroId", async(req,res)=>{
+    // res.json({msg: "Deleting Superhero " + req.params.superHeroId} );
+    try {
+        await Hero.findByIdAndDelete(req.params.superHeroId);
+        res.json({msg:"Hero Removed"})
+   } catch (error) {
+       console.log(error);
+    }
+
 })
 
 
